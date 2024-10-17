91f3232f7d75a2838d56680dddf5c254276f3df7
vandraaa <kevinandranugroho732@gmail.com>
2024-10-17 19:17:17 +0700

    Github Action for auto delete

--- backend/src/controllers/deletePhotoInTrash.js
+++ backend/src/controllers/deletePhotoInTrash.js
@@ -1,6 +1,6 @@
 const prisma = require("../config/database");
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = require('../../serviceAccountKey.json');
 const storageBucket = process.env.FIREBASE_STORAGE_BUCKET;
 
 admin.initializeApp({
