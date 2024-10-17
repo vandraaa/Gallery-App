64d2b235fb65dbf57c914701f2feafe0f418c129
vandraaa <kevinandranugroho732@gmail.com>
2024-10-17 18:56:58 +0700

    Github Action for auto delete

---
+++ backend/src/controllers/deletePhotoInTrash.js
@@ -0,0 +1,48 @@
+const prisma = require("./config/database");
+const admin = require('firebase-admin');
+const serviceAccount = require('./serviceAccountKey.json');
+const storageBucket = process.env.FIREBASE_STORAGE_BUCKET;
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: storageBucket
+});
+
+const runCronJob = async () => {
+    const now = new Date();
+    const fiveMinutesAgo = new Date(now.getTime() - 5 * 60 * 1000);
+    console.log('Cron job started at', now);
+
+    try {
+        const photos = await prisma.photo.findMany({
+            where: {
+                isDelete: true,
+                deletedAt: {
+                    gte: fiveMinutesAgo
+                }
+            }
+        });
+        console.log('Photos to delete:', photos);
+
+        const bucket = admin.storage().bucket();
+        for (const photo of photos) {
+            const fileName = photo.url.split('/o/')[1].split('?')[0];
+            const decodedFileName = decodeURIComponent(fileName);
+            const file = bucket.file(decodedFileName);
+
+            await file.delete();
+            console.log(`File deleted: ${decodedFileName}`);
+            
+            await prisma.photo.delete({
+                where: {
+                    photoId: photo.photoId
+                }
+            });
+            console.log(`Photo record deleted: ${photo.photoId}`);
+        }
+    } catch (e) {
+        console.error('Error in cron job:', e);
+    }
+};
+
+runCronJob();
