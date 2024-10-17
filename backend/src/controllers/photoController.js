9d96a476d4c8c42aea39ebca4fec479e003b6259
vandraaa <kevinandranugroho732@gmail.com>
2024-10-17 20:17:40 +0700

    hehe

--- backend/src/controllers/photoController.js
+++ backend/src/controllers/photoController.js
@@ -1,6 +1,7 @@
 const prisma = require("../config/database");
 const multer = require('multer');
 const admin = require('firebase-admin');
+const cron = require('node-cron');
 
 const serviceAccount = require('../../serviceAccountKey.json');
 const storageBucket = process.env.FIREBASE_STORAGE_BUCKET;
@@ -9,6 +10,43 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     storageBucket: storageBucket
 });
+
+// cron.schedule('5 * * * *', async () => {
+//     const now = new Date();
+//     const fiveMinutesAgo = new Date(now.getTime() - 5 * 60 * 1000);
+//     console.log('Cron job started at', now);
+
+//     try {
+//         const photos = await prisma.photo.findMany({
+//             where: {
+//                 isDelete: true,
+//                 deletedAt: {
+//                     gte: fiveMinutesAgo
+//                 }
+//             }
+//         });
+//         console.log('Photos to delete:', photos);
+
+//         const bucket = admin.storage().bucket();
+//         for (const photo of photos) {
+//             const fileName = photo.url.split('/o/')[1].split('?')[0];
+//             const decodedFileName = decodeURIComponent(fileName);
+//             const file = bucket.file(decodedFileName);
+
+//             await file.delete();
+//             console.log(`File deleted: ${decodedFileName}`);
+            
+//             await prisma.photo.delete({
+//                 where: {
+//                     photoId: photo.photoId
+//                 }
+//             });
+//             console.log(`Photo record deleted: ${photo.photoId}`);
+//         }
+//     } catch (e) {
+//         console.error('Error in cron job:', e);
+//     }
+// });
 
 const uploadImageToFirebase = async (file) => {
     try {
@@ -317,5 +355,5 @@ module.exports = {
     favoritePhoto,
     getFavoritePhoto,
     getTrashPhoto,
-    cron
+    // cron
 }
