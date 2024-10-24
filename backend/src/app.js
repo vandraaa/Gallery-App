afe486ec71a6112ff2a1a6ae80ee56bfebe8c1e9
vandraaa <kevinandranugroho732@gmail.com>
2024-10-24 20:57:13 +0700

    cron job

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const PORT = require("./config/server") || 4321;
 const cors = require('cors');
-const cronJob = require('./controllers/photoController').cron;
 
 const app = express();
 
